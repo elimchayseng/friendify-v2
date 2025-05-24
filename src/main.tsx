899104ea2b1201d8a2ff409d2f3f336b2caa94d9
Tres Oelze <toelze@gmail.com>
2025-05-24 14:58:54 -0700

    Inject analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Failed to find the root element')
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
 
 createRoot(rootElement).render(
+  <>
     <App />
-)
+    <Analytics />
+  </>
+);
